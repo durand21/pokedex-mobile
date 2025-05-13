0530a3efd151c028401ec0792a98cf97eadd4a3f
durand21 <rommendurand77@gmail.com>
2025-05-12 22:37:53 -0600

    se modifica el modelo y el servicio de para tRAER el tipo del pokemon si agua o fuego etc

--- lib/data/models/pokemon.model.dart
+++ lib/data/models/pokemon.model.dart
@@ -1,21 +1,28 @@
 class Pokemon {
   final String name;
   final String url;
+  final List<String> types;
 
-  const Pokemon({required this.name, required this.url});
+  const Pokemon({
+    required this.name,
+    required this.url,
+    this.types = const [], // por defecto vacío
+  });
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
     return Pokemon(name: json['name'], url: json['url']);
   }
 
-  /// Extrae el ID del Pokémon desde la URL
   int get id {
     final uri = Uri.parse(url);
     final segments = uri.pathSegments;
     return int.tryParse(segments[segments.length - 2]) ?? 0;
   }
 
-  /// Construye la URL de la imagen oficial
   String get imageUrl =>
       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png';
+
+  Pokemon copyWithTypes(List<String> types) {
+    return Pokemon(name: name, url: url, types: types);
+  }
 }
--- lib/data/services/pokeapi.service.dart
+++ lib/data/services/pokeapi.service.dart
@@ -17,9 +17,28 @@ class PokeApiService {
       final data = json.decode(response.body);
       final List results = data['results'];
 
-      return results.map((item) => Pokemon.fromJson(item)).toList();
+      final List<Pokemon> pokemons = [];
+      for (var item in results) {
+        final pokemon = Pokemon.fromJson(item);
+        final types = await fetchPokemonTypes(pokemon.url);
+        pokemons.add(pokemon.copyWithTypes(types));
+      }
+
+      return pokemons;
     } else {
       throw Exception('Error al cargar Pokémon');
+    }
+  }
+
+  Future<List<String>> fetchPokemonTypes(String detailUrl) async {
+    final response = await http.get(Uri.parse(detailUrl));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final types = data['types'] as List;
+      return types.map<String>((t) => t['type']['name'].toString()).toList();
+    } else {
+      return [];
     }
   }
 }
