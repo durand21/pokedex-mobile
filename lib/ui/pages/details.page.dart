43e9f47ce904cbeb17f30e64741eda451705a212
durand21 <rommendurand77@gmail.com>
2025-05-12 23:35:32 -0600

    app bar en la pagina de detalle del pokemon

--- lib/ui/pages/details.page.dart
+++ lib/ui/pages/details.page.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
+import '../widgets/poke_appbar.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pagina de detalles'),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
+      appBar: const PokeAppBar(),
       body: Text('Pagina de detalles'),
     );
   }
