6946ccdaf10840a741d04d580a999401d8bb998d
durand21 <rommendurand77@gmail.com>
2025-05-12 22:16:51 -0600

    incluimos el provider que escuche los cambios y me cargue los pokemones al entrar a la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'app.dart';
+import 'logic/pokemon.controller.dart';
 
 void main() {
-  runApp(const PokeApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => PokemonController()..loadPokemon(),
+      child: const PokeApp(),
+    ),
+  );
 }
