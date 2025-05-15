2489b8ace3f366e52245ed6d2d968ab6e97b9fd7
durand21 <rommendurand77@gmail.com>
2025-05-14 22:32:37 -0600

    Modificacion estetica de las tarjetas

--- lib/ui/widgets/pokemon_card.dart
+++ lib/ui/widgets/pokemon_card.dart
@@ -10,50 +10,70 @@ class PokemonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bgColor =
+    final baseColor =
         typeColors[pokemon.types.isNotEmpty ? pokemon.types[0] : 'normal'] ??
-        Colors.grey;
-    //var bgColorWithOpacity = bgColor.withValues(alpha: (0.81 * 255).toDouble());
-
-    var bgColorWithOpacity = bgColor;
+            Colors.grey;
+    final baseColorDark = Color.alphaBlend(Colors.black.withOpacity(0.2), baseColor);
 
     return GestureDetector(
       onTap: () {
         Navigator.pushNamed(context, '/details', arguments: pokemon);
       },
-      child: Card(
-        elevation: 4,
-        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        child: Container(
-          decoration: BoxDecoration(
-            color: bgColorWithOpacity,
-            borderRadius: BorderRadius.circular(12),
-          ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+        decoration: BoxDecoration(
+          color: baseColor, // fondo sólido por tipo
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: baseColorDark/*Colors.black87*/, width: 2), // borde estilo carta
+          boxShadow: [
+            BoxShadow(
+              color: baseColor,// Colors.black26,
+              blurRadius: 6,
+              offset: const Offset(2, 4),
+            ),
+          ],
+        ),
+        child: Padding(
           padding: const EdgeInsets.all(12),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              CachedNetworkImage(
-                imageUrl: pokemon.imageUrl,
-                placeholder:
-                    (context, url) => const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
+              Container(
+                width: 180,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/backgrounds/aura.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: baseColor.withOpacity(0.4),
+                      blurRadius: 20,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: CachedNetworkImage(
+                    imageUrl: pokemon.imageUrl,
+                    fit: BoxFit.contain,
+                    placeholder: (context, url) =>
+                    const CircularProgressIndicator(strokeWidth: 2),
+                    errorWidget: (context, url, error) =>
+                    const Icon(Icons.error),
+                  ),
+                ),
               ),
               const SizedBox(height: 12),
               Text(
                 pokemon.name.toUpperCase(),
                 style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 16,
                   color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 6),
-              Text(
-                'Ver más...',
-                style: Theme.of(context).textTheme.labelSmall?.copyWith(
-                  color: Colors.white70,
-                  fontStyle: FontStyle.italic,
                 ),
               ),
             ],
